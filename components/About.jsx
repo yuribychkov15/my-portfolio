e741ca8f449e2020574b10384678f0157cc3c0ad
Yuri Bychkov <yuribychkov15@gmail.com>
2025-06-30 14:28:14 -0400

    folder path fixed

--- components/About.jsx
+++ components/About.jsx
@@ -7,7 +7,7 @@ function About() {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div className="flex flex-col justify-center items-center"> 
                     <img 
-                        src="./public/assets/DSC01953.jpg"
+                        src="/assets/DSC01953.jpg"
                         className="w-64 h-64 rounded-full object-cover mb-10"
                     />
                     <h2 className="font-mono">
@@ -19,27 +19,27 @@ function About() {
                 </div>
                 <div className="grid grid-cols-3 items-center gap-x-4 sm:mt-10">
                     <div className="flex flex-col items-center">
-                        <img src="./assets/icons/html.png" className="pb-2 w-32 h-32"/>
+                        <img src="/assets/icons/html.png" className="pb-2 w-32 h-32"/>
                         <span className="font-mono">HTML</span>
                     </div>
                     <div className="flex flex-col items-center">
-                        <img src="./assets/icons/css.png" className="pb-2 w-32 h-32"/>
+                        <img src="/assets/icons/css.png" className="pb-2 w-32 h-32"/>
                         <span className="font-mono">CSS</span>
                     </div>
                     <div className="flex flex-col items-center">
-                        <img src="./assets/icons/js.png" className="pb-2 w-32 h-32"/>
+                        <img src="/assets/icons/js.png" className="pb-2 w-32 h-32"/>
                         <span className="font-mono">JS</span>
                     </div>
                     <div className="flex flex-col items-center">
-                        <img src="./assets/icons/react.png" className="pb-2 w-32 h-32"/>
+                        <img src="/assets/icons/react.png" className="pb-2 w-32 h-32"/>
                         <span className="font-mono">React</span>
                     </div>
                     <div className="flex flex-col items-center">
-                        <img src="./assets/icons/nodejs.png" className="pb-2 w-32 h-32"/>
+                        <img src="/assets/icons/nodejs.png" className="pb-2 w-32 h-32"/>
                         <span className="font-mono">NodeJS</span>
                     </div>
                     <div className="flex flex-col items-center">
-                        <img src="./assets/icons/git.png" className="pb-2 w-32 h-32"/>
+                        <img src="/assets/icons/git.png" className="pb-2 w-32 h-32"/>
                         <span className="font-mono">Git</span>
                     </div>
                 </div>
